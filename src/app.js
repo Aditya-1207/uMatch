ea6228f2708702c0f8c07a3bea264d239ca79805
Aditya-1207 <kulkarni.aditya12@gmail.com>
2026-03-28 16:57:24 +0530

    Tutorial3

--- src/app.js
+++ src/app.js
@@ -8,24 +8,43 @@ app.listen("1207",()=>{
     console.log("Server is listening on 1207");
 });
 
-//Request handlers
+//Tutorial 2: Request handlers
+// app.get("/profile",(req,res)=>{
+//     res.send({firstName:"Aditya",lastName:"Dhar"});
+// });
 
-app.get("/profile",(req,res)=>{
-    res.send({firstName:"Aditya",lastName:"Dhar"});
-});
+// app.post("/profile",(req,res)=>{
+//     res.send("Profile created successfully")
+// });
 
-app.post("/profile",(req,res)=>{
-    res.send("Profile created successfully")
-});
+// app.put("/profile",(req,res)=>{
+//     res.send("Profile updated successfull")
+// });
 
-app.put("/profile",(req,res)=>{
-    res.send("Profile updated successfull")
-});
+// app.delete("/profile",(req,res)=>{
+//     res.send("Profile deleted successfully")
+// });
 
-app.delete("/profile",(req,res)=>{
-    res.send("Profile deleted successfully")
-});
+// app.use("/",(req,res) =>{
+//     res.send("This is from parent request handler");
+// });
 
-app.use("/",(req,res) =>{
-    res.send("This is from parent request handler");
-});
+//Tutorial 3: Introdcution to sequence of route handlers and Next()
+app.get("/profile",
+    [(req,res,next)=>{
+            console.log("Into GET Profile API");
+        //    res.send({firstName:"Aditya",lastName:"Kulkarni"});
+        next();
+        
+        },
+        (req,res)=>{
+            console.log("This is inside arrat 2nd route");
+            res.send("This is inside array 2nd route");
+        }
+    ],
+    (req,res,next)=>{
+        console.log("Into second function outside array");
+       res.send("This comes from second block");
+       //next();
+    }
+);
